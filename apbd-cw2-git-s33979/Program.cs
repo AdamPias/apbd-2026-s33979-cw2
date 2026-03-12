64d99a49626959d297f42b5b2aa08cc369003040
Adam Cwikla <adamcwikla04@gmail.com>
2026-03-12 16:07:22 +0100

    zmiana tej samej nazwy metody co w featureconflict

--- apbd-cw2-git-s33979/Program.cs
+++ apbd-cw2-git-s33979/Program.cs
@@ -4,14 +4,14 @@ Console.WriteLine("Hello, World !!!!!!!");
 
 int a = 5;
 
-static double CalculateAvg(int[] values)
+static double CalculateAvg420(int[] values)
 {
     return values.Sum()/ values.Length;
 }
 
 int[] tab = new[] { a, 5, 5 };
 
-Console.WriteLine(CalculateAvg(tab));
+Console.WriteLine(CalculateAvg420(tab));
 Console.WriteLine("YOoooooo to jest main commit");
 
 
